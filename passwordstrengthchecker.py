9da38fe8d32355fbb82a442db3fc478a611dc2b3
Raksha S <rakshareddy18s@gmail.com>
2026-09-04 12:30:38 +0530

    Add password strength checker functionality

---
+++ passwordstrengthchecker.py
@@ -0,0 +1,8 @@
+password = input("Enter a password: ")
+
+if len(password) < 6:
+    print("Weak Password")
+elif len(password) < 10:
+    print("Medium Password")
+else:
+    print("Strong Password")
